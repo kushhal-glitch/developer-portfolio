1c5abf04a0fad02a49dee0f108b294c9f7da23c6
kushhal-glitch <69195245+kushhal-glitch@users.noreply.github.com>
2023-07-18 21:07:13 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 
 export const headerData = {
-    name: 'ABU SAID',
-    title: "Hello! I am",
-    desciption: "Hello! I am ABU SAID, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+    name: 'KUSHHAL',
+    title: "Hey! I am",
+    desciption: "Hey! I am Kushhal, As a passionate technologist and innovator, my mission is centered around creating and promoting technologies, systems, and movements that foster hyperlocal resilience, self-sustainability, and dynamic growth",
+    image: 'https://i.ibb.co/BZMNkF1/Whats-App-Image-2023-07-18-at-8-56-53-PM.jpg',
+    imagebw: 'https://i.ibb.co/GP9rg3y/d9ea1840-0c65-40fa-b36d-8925294ed352.jpg',
     resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
 }
