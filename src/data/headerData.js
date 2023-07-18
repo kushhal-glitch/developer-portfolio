85315cac9cf943d9249a2d165495e58342d896da
kushhal-glitch <69195245+kushhal-glitch@users.noreply.github.com>
2023-07-19 00:30:45 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     title: "Hey! I am",
     desciption: "Hey! I am Kushhal, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: 'https://i.ibb.co/GP9rg3y/d9ea1840-0c65-40fa-b36d-8925294ed352.jpg',
-    imagebw: 'https://i.ibb.co/GP9rg3y/d9ea1840-0c65-40fa-b36d-8925294ed352.jpg',
+    imagebw: 'https://i.ibb.co/y6DzPtC/My-project-1.jpg',
     resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
 }
